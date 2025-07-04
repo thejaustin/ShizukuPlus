274fdc8c314fe46d26f0f1dd09e5de5ef7cf4287
Ryfter <ryfterwastaken@gmail.com>
2025-07-04 15:02:11 +0100

    better

--- manager/src/main/java/moe/shizuku/manager/service/AdbStartService.kt
+++ manager/src/main/java/moe/shizuku/manager/service/AdbStartService.kt
@@ -1,20 +1,25 @@
 package moe.shizuku.manager.service
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.ServiceInfo
 import android.net.ConnectivityManager
-import android.net.Network
 import android.net.NetworkCapabilities
-import android.net.NetworkRequest
 import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.IBinder
 import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.ServiceCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -29,7 +34,9 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
 class AdbStartService : Service() {
-    lateinit var cm: ConnectivityManager
+
+    lateinit var notification: Notification
+    var receiver: BroadcastReceiver? = null
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -39,34 +46,44 @@ class AdbStartService : Service() {
     override fun onCreate() {
         super.onCreate()
 
-        cm = getSystemService(ConnectivityManager::class.java)
+        Log.d(TAG, "onCreate")
 
-        if (checkWifiConnected()) {
-            startShizuku(applicationContext)
-            return
-        }
-
-        val receiver = object : BroadcastReceiver() {
+        receiver = object : BroadcastReceiver() {
+            @RequiresApi(Build.VERSION_CODES.TIRAMISU)
             override fun onReceive(context: Context?, intent: Intent?) {
+
                 if (context == null) return
 
                 if (checkWifiConnected()) {
                     startShizuku(context)
-                    unregisterReceiver(this)
                 }
             }
         }
+
         val intentFilter = IntentFilter(WifiManager.NETWORK_STATE_CHANGED_ACTION)
 
-        applicationContext.registerReceiver(receiver, intentFilter)
+        this.registerReceiver(receiver, intentFilter)
+
+        // this is after registering the receiver, so in case it fails, so the receiver is still active
+        if (checkWifiConnected()) startShizuku(this)
     }
+
+
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         return START_STICKY
     }
 
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        receiver?.let { unregisterReceiver(it) }
+        super.onDestroy()
+    }
+
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     private fun startShizuku(context: Context) {
+
+        Log.d(TAG, "startShizuku")
         Toast.makeText(context, R.string.notification_service_starting, Toast.LENGTH_SHORT).show()
 
         val cr = context.contentResolver
@@ -82,6 +99,7 @@ class AdbStartService : Service() {
                     val keystore = PreferenceAdbKeyStore(ShizukuSettings.getPreferences())
                     val key = AdbKey(keystore, "shizuku")
                     val client = AdbClient("127.0.0.1", port, key)
+
                     client.connect()
                     client.shellCommand(Starter.internalCommand, null)
                     client.close()
@@ -102,7 +120,14 @@ class AdbStartService : Service() {
     }
 
     private fun checkWifiConnected() : Boolean {
+        val cm = getSystemService(ConnectivityManager::class.java)
         return cm.getNetworkCapabilities(cm.activeNetwork)
             ?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) == true
+    }
+
+    companion object {
+        const val TAG = "AdbStartService"
+        const val NOTIFICATION_CHANNEL = "wadb_service"
+        const val SERVICE_ID = 1447
     }
 }
