2cac8474a3fd68994c134306294839c7bcf22c78
thedjchi <achiommino@gmail.com>
2025-11-06 11:32:02 -0500

    update manifest special foreground service justification

--- manager/src/main/java/moe/shizuku/manager/receiver/NotifCancelReceiver.kt
+++ manager/src/main/java/moe/shizuku/manager/receiver/NotifCancelReceiver.kt
@@ -1,10 +1,10 @@
 package moe.shizuku.manager.receiver
 
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.work.WorkManager
-import android.app.NotificationManager
 
 class NotifCancelReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
