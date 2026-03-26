5484aa8b8e24d27cac2722140f96eefe095ebb80
thejaustin <the.j.austin@gmail.com>
2026-03-26 09:57:35 -0500

    Fix CI: Add missing R import in ServiceDoctorActivity

--- manager/src/main/java/moe/shizuku/manager/settings/ServiceDoctorActivity.kt
+++ manager/src/main/java/moe/shizuku/manager/settings/ServiceDoctorActivity.kt
@@ -20,6 +20,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import moe.shizuku.manager.R
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
